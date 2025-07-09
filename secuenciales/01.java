ba2eb7cb28b614a0817f787cd3e7d3ae70b632bc
jhomarlino47470166 <jhomarlino@gmail.com>
2025-07-08 22:49:33 -0500

    Update 01.java

--- secuenciales/01.java
+++ secuenciales/01.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class 01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Varones: ");
+        int varones = scanner.nextInt();
+
+        System.out.print("Mujeres: ");
+        int mujeres = scanner.nextInt();
+
+        // Aquí puedes hacer algo con los datos, por ejemplo:
+        int total = varones + mujeres;
+        System.out.println("Total de estudiantes: " + total);
+    }
+
+}
 
